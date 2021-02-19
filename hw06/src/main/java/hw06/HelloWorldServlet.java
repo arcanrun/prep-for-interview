3021364565341a734b10a70441a384de474051d9
Назин Аркадий <arkadii.nazin@simbirsoft.com>
2021-02-19 23:49:16 +0300

    hello world servlet

--- hw06/src/main/java/hw06/HelloWorldServlet.java
+++ hw06/src/main/java/hw06/HelloWorldServlet.java
@@ -11,6 +11,6 @@ import java.io.IOException;
 public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().printf("<h1>%s</h1>", "Hello world!");
+        resp.getWriter().printf("<h1>%s</h1>", "Hello world!!!");
     }
 }
