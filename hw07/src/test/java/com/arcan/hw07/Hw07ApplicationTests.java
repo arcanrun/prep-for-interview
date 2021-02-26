b65040712041b49eff89fef734b4e51b1c1a5296
Назин Аркадий <arkadii.nazin@simbirsoft.com>
2021-02-27 01:31:34 +0300

    init

---
+++ hw07/src/test/java/com/arcan/hw07/Hw07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcan.hw07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
